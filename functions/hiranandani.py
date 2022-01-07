1f37bf4f27373f6cc137a0e82c4efd713290c555
subburajan-perumal <subburajanperumal@gmail.com>
2022-01-07 13:51:31 +0530

    Update hiranandani.py

--- functions/hiranandani.py
+++ functions/hiranandani.py
@@ -28,11 +28,25 @@ def fin_function(lead_id, sub_project_name, first_name, last_name, phone, email1
     if sub_project_name == "Hiranandani Parks":
         sub_project_name = 'Hiranandani Parks'
 
-    if '+91' not in phone:
+    '''if '+91' not in phone:
         print("Not Indian Number")
         return "Failed"
 
-    phone = phone[3:]        
+    phone = phone[3:]
+    '''
+    #The above execuation we used to do when we wanted to accept only indian number but now we are expainding it for many country.
+    
+    country_code = [+91,+1,+44,+93,+880,+501,+55,+33,+49,+98,+39,+81,+962,+965,+60,+64,+65,+34,+94,+971]
+    k = 0
+    for i in country_code:
+        #print('+'+str(i), ' ', phone[:len('+'+str(i))])
+        if '+'+str(i)== phone[:len('+'+str(i))]:
+            country_code = phone[:len('+'+str(i))]
+            phone = phone[len('+'+str(i)):]
+            k=1
+     if k==0:
+        return('the phonne number does not have the proper country code')
+
 
     s=Service(constants.engine)
     browser=webdriver.Firefox(service=s)
@@ -47,6 +61,11 @@ def fin_function(lead_id, sub_project_name, first_name, last_name, phone, email1
 
         name  = browser.find_element(By.NAME, "Pname")
         name.send_keys(name1)
+        
+        #Adding countr code
+        country=  browser.find_element(By.NAME, 'cc')
+        country   = Select(country)
+        country.select_by_value(country_code[1:])
 
         contact=browser.find_element(By.NAME,'pmob')
         contact.send_keys(phone)
