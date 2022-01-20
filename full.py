3ec3b3de684fc0ae4677173927ab3217bc7f8f7d
subburajan-perumal <subburajanperumal@gmail.com>
2022-01-20 23:55:43 +0530

    upload to test server

--- full.py
+++ full.py
@@ -1,4 +1,4 @@
-import imp
+
 from flask import Flask,request,Response
 import queue
 app=Flask(__name__)
@@ -6,13 +6,18 @@ app=Flask(__name__)
 request_queue=queue.PriorityQueue(50)
 
 def queue_request(request_data,priority):
-    if request_queue.qsize>0:
-        request_queue.put(priority,request_data)
+    print(request_queue.qsize())
+    request_queue.put((priority,request_data))
+    while request_queue.qsize()!=0:
+        print(f"{request_queue.qsize()} {request_queue.get()}")
+
+
 @app.post('/')
 def webhook():
-
     queue_request(priority=5,request_data=request.headers)
     # print(request.headers)
+    return Response("Success",status=200)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",debug=True, port = 8686)
