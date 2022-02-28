4ee638d922f93cfd8a30e4a3b4aa3e9d4398fe77
subburajan-perumal <subburajanperumal@gmail.com>
2022-02-28 23:45:44 +0530

    add interest properties  lead generation

--- app/functions/finder.py
+++ app/functions/finder.py
@@ -15,8 +15,8 @@ def function_finder(lead_data):
         DB = CONN['lead_automation']
         LEADS=DB['leads']
         SITE=DB['Site']
-        print(SITE)
-        print("db working")
+        # print(SITE)
+        # print("db working")
         
     except Exception as e:
         print("Error occured due to "+str(e))
@@ -25,7 +25,7 @@ def function_finder(lead_data):
         user_detail=LEADS.find_one({"email":lead_data["email"],"phone":lead_data["phone"]})
         print(user_detail)
         fullname=lead_data['first_name']+lead_data['last_name']
-        if not user_detail:
+        if user_detail is None:
             lead_creation={"name":fullname,
                     "phone":lead_data["phone"],
                     "email":lead_data["email"], 
@@ -34,42 +34,43 @@ def function_finder(lead_data):
                     }
             LEADS.insert_one(lead_creation)
     
-        project_name=lead_data.get("project_enquired_for")
-        print("working before using db")
+        if "project_enquired_for" in lead_data:
+            project_name=lead_data.get("project_enquired_for")
+            print("working before using db")
 
-        
-        site=SITE.find_one({"key_words":{"$in":[project_name]}})
-        if site:
-            browser_automation = SiteAutomator( lead_data[ "phone" ] , lead_data[ "email" ],lead_data)
-            browser_automation.projectCheck( site ['name'], project_name)
-            browser_automation.automated_flow()
-            browser_automation.upload_data()
-            browser_automation.teardown()
-            
-
-        
-        # site=SITE.find_one({"key_words":{"$in":["Akshaya Tango"]}}) 
-        # print("site name :",site," ",)
-
-        # LEADS.insert_one()
-        # if not site:
-            # return "failed"
-            # pass
-        # else:
-            # dyn_mod=import_module("."+site['name'],"app.functions")
-        
+            #Enquriedsite
+            site=SITE.find_one({"key_words":{"$in":[project_name]}})
+            if site is not None:
+                browser_automation = SiteAutomator( lead_data[ "phone" ] , lead_data[ "email" ],lead_data)
+                browser_automation.projectCheck( site ['name'], project_name,True)
+                browser_automation.automated_flow()
+                browser_automation.upload_data()
+                browser_automation.teardown()
+                del browser_automation
+                
         storage="./storage/"
     
-        #project_enquire_for
-        # dyn_mod.addlead(site['name'],sub_project_name=project_name,storage=storage,**lead_data)
-        # if "interested_properties" in lead_data:
+        # Interestsite
+        if "interested_properties" in lead_data:
+            if lead_data['interested_properties'] != "":
+                for interested_site in lead_data["interested_properties"].split(";"):
+                    project_name=interested_site
+                    # print("working before using db")
+                    site=SITE.find_one({"key_words":{"$in":[project_name]}})
+                    browser_automation = SiteAutomator( lead_data[ "phone" ] , lead_data[ "email" ],lead_data)
+                    browser_automation.projectCheck( site ['name'], interested_site,True)
+                    browser_automation.automated_flow()
+                    browser_automation.upload_data()
+                    browser_automation.teardown()
+                    del browser_automation
 
-        #     for interested_site in lead_data["interested_properties"].split(";"):
-        #         site=SITE.find_one({"key_words":{"$in":[interested_site]}})
-        #         if not site:
-        #             continue
-        #         dyn_mod=import_module("."+site['name'],"app.functions")
-        #         dyn_mod.addlead(site['name'],sub_project_name=interested_site,storage=storage,**lead_data)
+
+        if "interested_localities" in lead_data:
+            if lead_data["interested_localities"]!= "":
+                for localities in lead_data['interested_localities'].split(";"):
+                    site=SITE.find_one({"location":localities})
+                
+                    pass
         # # for place in lead_data["localities"]:
         #     # pass
         # #bylocalities
