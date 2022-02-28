744a8e81348393907523d169afe8f41856fec756
subburajan-perumal <subburajanperumal@gmail.com>
2022-02-28 11:15:59 +0530

    lead reverted to  funcition based

--- app/tasks.py
+++ app/tasks.py
@@ -1,7 +1,9 @@
 # from curses.ascii import isalnum, isalpha
+from unittest import result
 from celery import Celery
 from app.functions.finder import  function_finder
 from app.config import DevelopmentConfig
+from app.functions.leadautomator import LeadAutomator
 # from app.util.utility import cmpstring
 import time
 import json
@@ -18,6 +20,13 @@ def lead(**lead_data):
         # print(os.getenv())
         print("celery started")
         result=function_finder(lead_data)
+        # lead_sender=LeadAutomator(lead_data)
+        # lead_sender.create_lead()
+        # lead_sender.generate_lead("enquired")
+        # lead_sender.generate_lead("interested")
+        # lead_sender.generate_lead("location")
+        result="success"
+    
         return result
     except:
         return "failed"
@@ -25,10 +34,10 @@ def lead(**lead_data):
 @celery_app.task
 def bulk_lead(*args):
     try:
-        f = open("bulk_lead.json","a")
-        f.write(str(args))
-        f.close()
-        time.sleep(10)
+        # f = open("bulk_lead.json","a")
+        # f.write(str(args))
+        # f.close()
+        # time.sleep(10)
         return "success" 
     except:
         return "failed"
