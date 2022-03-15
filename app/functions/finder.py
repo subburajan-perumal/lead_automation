14c5688ad52c2ae78b8d919edb977187e3d08d83
subburajan-perumal <subburajanperumal@gmail.com>
2022-03-15 13:39:02 +0530

    modified project enquire for query

--- app/functions/finder.py
+++ app/functions/finder.py
@@ -1,100 +1,135 @@
 from datetime import datetime
 from pymongo import MongoClient
-from app.functions.site_base import SiteAutomator   
+from app.functions.site_base import SiteAutomator
 import logging
 import os
-MONGO_DB="REDACTED"
+MONGO_DB = "REDACTED"
 
 
 def function_finder(lead_data):
     try:
-    #     logging.basicConfig("")
-    #     logger=logging.getLogger()
-    #     logger.level
-        CONN=MongoClient(MONGO_DB)
+        #     logging.basicConfig("")
+        #     logger=logging.getLogger()
+        #     logger.level
+        CONN = MongoClient(MONGO_DB)
         DB = CONN['lead_automation']
-        LEADS=DB['leads']
-        SITE=DB['Site']
+        LEADS = DB['leads']
+        SITE = DB['Site']
         # print(SITE)
         # print("db working")
-        
+
     except Exception as e:
         print("Error occured due to "+str(e))
-    
+
     try:
-        user_detail=LEADS.find_one({"email":lead_data["email"],"phone":lead_data["phone"]})
+        user_detail = LEADS.find_one(
+            {"email": lead_data["email"], "phone": lead_data["phone"]})
         print(user_detail)
-        fullname=lead_data['first_name']+lead_data['last_name']
+        fullname = lead_data['first_name']+lead_data['last_name']
         if user_detail is None:
-            lead_creation={"name":fullname,
-                    "phone":lead_data["phone"],
-                    "email":lead_data["email"], 
-                    "created_at":datetime.now(),
-                    "modified_time":datetime.now()
-                    }
+            lead_creation = {"name": fullname,
+                             "phone": lead_data["phone"],
+                             "email": lead_data["email"],
+                             "created_at": datetime.now(),
+                             "modified_time": datetime.now()
+                             }
             LEADS.insert_one(lead_creation)
-        #Enquired site
+        # Enquired site
         if "project_enquired_for" in lead_data:
-            project_name=lead_data.get("project_enquired_for")
+            project_name = lead_data.get("project_enquired_for")
             print("working before using db")
 
-            #Enquriedsite
-            site=SITE.find_one({"key_words":{"$in":[project_name]}})
-            if site is not None:
-                browser_automation = SiteAutomator( lead_data[ "phone" ] , lead_data[ "email" ],lead_data)
-                browser_automation.projectCheck( site ['name'], project_name,True)
+            # Enquriedsite
+
+            filter = {"project_list.keywords": project_name}
+            site_list=  DB.Site.aggregate(
+                [
+                    {
+                        "$match":
+                            {
+                                "project_list.keywords":project_name,
+                                "status":1
+                            }
+                    },
+                
+                    {
+                        "$unwind":"$project_list"
+                    },
+                    {
+                        "$match":{"project_list.keywords":project_name}
+                    }
+                ]
+            )
+
+            for site_data in site_list:
+                site_name = site_data["name"]
+                project_name = site_data["project_list"]["project_name"]
+
+                # site=SITE.find_one({"project_list.keywords":project_name},{"name":1,"project_list.project_name.$":1})
+                # print(site)
+                # if site is not None:
+                browser_automation = SiteAutomator(
+                                                    lead_data["phone"],
+                                                    lead_data["email"],
+                                                    lead_data,site_data
+                                                )
+                browser_automation.projectCheck(site_name, project_name, False)
                 browser_automation.automated_flow()
                 browser_automation.upload_data()
                 browser_automation.teardown()
                 del browser_automation
-                
-        storage="./storage/"
-    
+
+        storage = "./storage/"
+
         # Interestsite
         if "interested_properties" in lead_data:
             if lead_data['interested_properties'] != "":
                 for interested_site in lead_data["interested_properties"].split(";"):
-                    project_name=interested_site
+                    project_name = interested_site
                     # print("working before using db")
-                    
-                    site=SITE.find_one({"key_words":{"$in":[project_name]}})
+
+                    site = SITE.find_one(
+                        {"key_words": {"$in": [project_name]}})
                     if site is not None:
-                        browser_automation = SiteAutomator( lead_data[ "phone" ] , lead_data[ "email" ],lead_data)
-                        browser_automation.projectCheck( site ['name'], interested_site,True)
+                        browser_automation = SiteAutomator(
+                            lead_data["phone"], lead_data["email"], lead_data)
+                        browser_automation.projectCheck(
+                            site['name'], interested_site, True)
                         browser_automation.automated_flow()
                         browser_automation.upload_data()
                         browser_automation.teardown()
                         del browser_automation
 
-        #interestlocaties
+        # interestlocaties
         if "interested_localities" in lead_data:
-            if lead_data["interested_localities"]!= "":
+            if lead_data["interested_localities"] != "":
                 for localities in lead_data['interested_localities'].split(";"):
-                    site_list=SITE.find({"location.location_name":localities},{"name":1,"location.project_name":1})
+                    site_list = SITE.find({"location.location_name": localities}, {
+                                          "name": 1, "location.project_name": 1})
                     if site_list is not None:
                         for single_site in site_list:
                             # print()
-                        # print(single_site['name'])
-                        # print(single_site["location"][0]["project_name"])
-                            site_name=(single_site['name'])
-                            project=single_site["location"][0]["project_name"]
-                            browser_automation = SiteAutomator( lead_data[ "phone" ] , lead_data[ "email" ],lead_data)
-                            browser_automation.projectCheck(site_name,project,False)
+                            # print(single_site['name'])
+                            # print(single_site["location"][0]["project_name"])
+                            site_name = (single_site['name'])
+                            project = single_site["location"][0]["project_name"]
+                            browser_automation = SiteAutomator(
+                                lead_data["phone"], lead_data["email"], lead_data)
+                            browser_automation.projectCheck(
+                                site_name, project, False)
                             browser_automation.automated_flow()
                             browser_automation.upload_data()
                             browser_automation.teardown()
                             del browser_automation
 
-                    
-                
                     pass
         # # for place in lead_data["localities"]:
         #     # pass
         # #bylocalities
 
-            #omr-akshaya tango
+            # omr-akshaya tango
         return "success"
     except Exception as e:
-        print("error occured in function_finder",str(e))
+        print("error occured in function_finder", str(e))
         # print(os.getcwd())
         return "failed"
