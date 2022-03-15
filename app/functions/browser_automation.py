4723c0e40dda02ae36c97706800284d45ed763b5
subburajan-perumal <subburajanperumal@gmail.com>
2022-03-15 18:38:50 +0530

    added alignment

--- app/functions/browser_automation.py
+++ app/functions/browser_automation.py
@@ -16,12 +16,12 @@ logging.basicConfig(
     filename= "browser.log",
     level= logging.INFO,
     format= f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s',
-    encoding='utf-8',
-    filemode="w"
+    encoding= 'utf-8',
+    filemode= "w"
     )
 
 
-def akshaya(subproject, browser, site_data, lead_data,automate_path):
+def akshaya(subproject:str, browser, site_data:dict, lead_data:dict,automate_path:list):
     try:
         save_path = automate_path
         fullname=str(lead_data['name'])
@@ -80,66 +80,66 @@ def akshaya(subproject, browser, site_data, lead_data,automate_path):
         return -1
 
 
-def brigade(subproject, browser, site_data, lead_data,automate_path):         
+def brigade(subproject:str, browser, site_data:dict, lead_data:dict,automate_path:list):         
     try:    
         
         save_path = automate_path
-        fullname=str(lead_data['name'])
-        first_name,last_name=getName(fullname)
+        fullname= str(lead_data['name'])
+        first_name, last_name = getName(fullname)
         
     #browser# yield "on working"
-        save_path=automate_path
+        save_path= automate_path
         browser.get(site_data["url"])
-        company_name = browser.find_element(By.XPATH,'//*[@id="input_5"]')
+        company_name = browser.find_element(By.XPATH, '//*[@id="input_5"]')
         company_name.send_keys(site_data["company"])
         #agent name
-        f_agentf = browser.find_elements(By.XPATH,'//*[@id="first_6"]')
+        f_agentf = browser.find_elements(By.XPATH, '//*[@id="first_6"]')
         f_agentf[0].send_keys(site_data["agentf1"])
-        f_agentl = browser.find_elements(By.XPATH,'//*[@id="last_6"]')
+        f_agentl = browser.find_elements(By.XPATH, '//*[@id="last_6"]')
         f_agentl[0].send_keys(site_data["agentl1"])
         
         #agent mobile number
-        f_aph11 = browser.find_elements(By.XPATH,'//*[@id="input_7_country"]')
+        f_aph11 = browser.find_elements(By.XPATH, '//*[@id="input_7_country"]')
         f_aph11[0].send_keys(site_data["aph1"])
-        f_aph21 = browser.find_elements(By.XPATH,'//*[@id="input_7_area"]')
+        f_aph21 = browser.find_elements(By.XPATH, '//*[@id="input_7_area"]')
         f_aph21[0].send_keys(site_data["aph2"])
-        f_aph31 = browser.find_elements(By.XPATH,'//*[@id="input_7_phone"]')
+        f_aph31 = browser.find_elements(By.XPATH, '//*[@id="input_7_phone"]')
         f_aph31[0].send_keys(site_data["aph3"])
         
         #agent email
-        f_mail = browser.find_elements(By.XPATH,'//*[@id="input_8"]')
+        f_mail = browser.find_elements(By.XPATH, '//*[@id="input_8"]')
         f_mail[0].send_keys(site_data["email"])
         
         #client name
-        f_firstname=browser.find_element(By.XPATH,'//*[@id="first_11"]')
+        f_firstname= browser.find_element(By.XPATH, '//*[@id="first_11"]')
         f_firstname.send_keys(first_name)
-        f_lastname = browser.find_element(By.ID,'last_11')
+        f_lastname= browser.find_element(By.ID, 'last_11')
         f_lastname.send_keys(last_name)
         
         #mobile number 1
-        f_aph1c = browser.find_element(By.XPATH,'//*[@id="input_12_country"]')
+        f_aph1c = browser.find_element(By.XPATH, '//*[@id="input_12_country"]')
         f_aph1c.send_keys(site_data["aph1"])
-        f_aph2c = browser.find_element(By.XPATH,'//*[@id="input_12_area"]')
+        f_aph2c = browser.find_element(By.XPATH, '//*[@id="input_12_area"]')
         f_aph2c.send_keys(site_data["aph2"])
-        f_aph3c = browser.find_element(By.XPATH,'//*[@id="input_12_phone"]')
+        f_aph3c = browser.find_element(By.XPATH, '//*[@id="input_12_phone"]')
         f_aph3c.send_keys(lead_data["phone"])
         
         #mobile number 2
-        f_aph1c2 = browser.find_element(By.XPATH,'//*[@id="input_14_country"]')
+        f_aph1c2 = browser.find_element(By.XPATH, '//*[@id="input_14_country"]')
         f_aph1c2.send_keys(site_data["aph1"])
-        f_aph2c2 = browser.find_element(By.XPATH,'//*[@id="input_14_area"]')
+        f_aph2c2 = browser.find_element(By.XPATH, '//*[@id="input_14_area"]')
         f_aph2c2.send_keys(site_data["aph2"])
-        f_aph3c2 = browser.find_element(By.XPATH,'//*[@id="input_14_phone"]')
+        f_aph3c2 = browser.find_element(By.XPATH, '//*[@id="input_14_phone"]')
         f_aph3c2.send_keys(lead_data["phone"])
         
         #emails
-        f_email11=browser.find_element(By.XPATH,'//*[@id="input_13"]')
+        f_email11= browser.find_element(By.XPATH, '//*[@id="input_13"]')
         f_email11.send_keys(lead_data["email"])
-        f_email21=browser.find_element(By.XPATH,'//*[@id="input_15"]')
+        f_email21= browser.find_element(By.XPATH, '//*[@id="input_15"]')
         f_email21.send_keys(lead_data["email"])
         
         #project name
-        project=Select(browser.find_element(By.XPATH,'//*[@id="input_16"]'))
+        project=Select(browser.find_element(By.XPATH, '//*[@id="input_16"]'))
         project.select_by_visible_text(subproject)
 
         browser.save_screenshot(save_path[0])    
@@ -162,29 +162,29 @@ def brigade(subproject, browser, site_data, lead_data,automate_path):
 def fomra(subproject, browser, site_data, lead_data, automate_path):
     try:
         save_path = automate_path
-        fullname=str(lead_data['name'])
-        first_name,last_name=getName(fullname)
+        fullname= str(lead_data['name'])
+        first_name, last_name = getName(fullname)
         browser.get(site_data['url'])
-        f_name=browser.find_element(By.XPATH,'/html/body/div/div/div/div/div/div/div/form/div[2]/div/div/input[1]')
+        f_name= browser.find_element(By.XPATH, '/html/body/div/div/div/div/div/div/div/form/div[2]/div/div/input[1]')
         f_name.send_keys(fullname)
-        f_email=browser.find_element(By.XPATH,'/html/body/div/div/div/div/div/div/div/form/div[3]/div/div/input')
+        f_email= browser.find_element(By.XPATH, '/html/body/div/div/div/div/div/div/div/form/div[3]/div/div/input')
         f_email.send_keys(lead_data['email'])
-        f_phone=browser.find_element(By.XPATH,'//html/body/div/div/div/div/div/div/div/form/div[4]/div/div/div/input')
+        f_phone= browser.find_element(By.XPATH, '//html/body/div/div/div/div/div/div/div/form/div[4]/div/div/div/input')
         f_phone.send_keys(lead_data['phone'])
-        f_project=Select(browser.find_element(By.XPATH,'/html/body/div/div/div/div/div/div/div/form/div[7]/div/div/select'))
+        f_project= Select(browser.find_element(By.XPATH, '/html/body/div/div/div/div/div/div/div/form/div[7]/div/div/select'))
         f_project.select_by_visible_text(site_data["partner_name"])
         
-        f_channel_pn=Select(browser.find_element(By.XPATH,'/html/body/div/div/div/div/div/div/div/form/div[5]/div/div/select'))
+        f_channel_pn= Select(browser.find_element(By.XPATH, '/html/body/div/div/div/div/div/div/div/form/div[5]/div/div/select'))
         f_channel_pn.select_by_visible_text(subproject)
         
-        f_channel_phone=browser.find_element(By.XPATH,'/html/body/div/div/div/div/div/div/div/form/div[8]/div/div/input')
+        f_channel_phone= browser.find_element(By.XPATH, '/html/body/div/div/div/div/div/div/div/form/div[8]/div/div/input')
         f_channel_phone.send_keys(site_data["channel_phone_number"])
 
         
         browser.save_screenshot(save_path[0])    
     
 
-        f_submit=browser.find_element(By.XPATH,'/html/body/div/div/div/div/div/div/div/form/div[9]/div/div/input').click()
+        f_submit= browser.find_element(By.XPATH, '/html/body/div/div/div/div/div/div/div/form/div[9]/div/div/input').click()
         time.sleep(5)
         # browser.implicitly_wait(5)
 
