07d8a036f425a68bffcade1f2955b9cb7e485ca1
subburajan-perumal <subburajanperumal@gmail.com>
2022-03-15 18:40:25 +0530

    add errorhandler view

--- app/views/error_handler.py
+++ app/views/error_handler.py
@@ -1,7 +1,13 @@
-# from flask import Blueprint
+from flask import Blueprint,render_template
 
-# #error handling methods
-# errorHandler=Blueprint("errorHandler",__name__)
+#error handling methods
+errorHandler=Blueprint("errorHandler",__name__)
+
+@errorHandler.app_errorhandler(404)
 # @errorHandler.errorhandler(404)
-# def routenotfound(e):
-#     return "route not found",404
+def routenotfound(error):
+    return "route not found", 404
+
+@errorHandler.app_errorhandler(405)
+def methodNotallowed(error):
+    return render_template("base.html")
