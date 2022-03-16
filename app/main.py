4600014b719c875bf21db27aa4586561c89d04cd
subburajan-perumal <subburajanperumal@gmail.com>
2022-03-16 14:24:45 +0530

    add error handler site

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from flask import Flask
 # from app.views.error_handler import errorHandler,badrequest_handler
 from app.views.home import home
+from app.views.error_handler import errorHandler
 from app.config import DevelopmentConfig
-
+from app.views.site import site
 # import tasks
 
 
@@ -10,7 +11,8 @@ def create_app():
     app=Flask(__name__)
     app.config.from_object(DevelopmentConfig())
     app.register_blueprint(home)
-    # app.register_blueprint(errorHandler)
+    app.register_blueprint(errorHandler)
+    # app.register_blueprint(site)
     # app.register_error_handler(errorHandler,badrequest_handler)
     return app
     
