5799bc00601972fcbc2f5447a610e9bea0b75bd0
subburajan-perumal <subburajanperumal@gmail.com>
2022-03-29 13:38:38 +0530

    add pep8 standards

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+"""
+This module contain the flask  webpages and error handling site
+and creata a app
+"""
 from flask import Flask
 # from app.views.error_handler import errorHandler,badrequest_handler
 from app.views.home import home
@@ -8,18 +12,15 @@ from app.views.site import site
 
 
 def create_app():
+    """_summary_
+    This is a function create a flask app
+    Returns:
+        class: return a flask class
+    """
     app=Flask(__name__)
     app.config.from_object(DevelopmentConfig())
     app.register_blueprint(home)
     app.register_blueprint(errorHandler)
-    # app.register_blueprint(site)
+    app.register_blueprint(site)
     # app.register_error_handler(errorHandler,badrequest_handler)
     return app
-    
-# app=create_app()
-
-
-
-
-
-    
