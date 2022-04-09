d2c47b2005c02296e1ccf0be3caf575feed2725b
subburajan-perumal <subburajanperumal@gmail.com>
2022-04-09 21:35:21 +0530

    updated run.py

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
-from app import main
+from app import create_app
 
 
-app=main.create_app()
-app.run(host="0.0.0.0",port=443,ssl_context=("domain.crt","domain.key"))
+app=create_app("development")
+
+if __name__== '__main__':
+    app.run()
+
+
+
+# celery_app=make_celery(app)
+# app.run(host="0.0.0.0",port=443,ssl_context=("domain.crt","domain.key"))
 
 # app.run("0.0.0.0")
 # app.run("0.0.0.0",8000)
-# app.run()
+# app.run(debug=True)
 # app.run(threaded=True)
 # app.run(host="0.0.0.0",port=8686)
 
