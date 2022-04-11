879b1b5508f6a94ab0f0d3fdfeaa23f8d3d2d3dc
subburajan-perumal <subburajanperumal@gmail.com>
2022-04-11 17:46:42 +0530

    added gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,13 @@
 from config import Config
+
 bind= "0.0.0.0:5000"
-workers= 2
+workers= 1
 accesslog= Config.LOG_PATH+"gunicorn.log"
 wsgi_app= "run:app"
 access_log_format='%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 errorlog= Config.LOG_PATH+"gunicorn_error.log"
 loglevel="debug"
-# proc_name="lead_automation_server"
+proc_name="lead_automation_server"
 ssl=""
 certfile=""
 capture_output= True
