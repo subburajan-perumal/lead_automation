27ee4e88d8a7afacaf99ad3ea4c5f647e2ef1eb7
subburajan-perumal <subburajanperumal@gmail.com>
2022-04-11 17:47:14 +0530

    added sentry log for flask

--- run.py
+++ run.py
@@ -1,12 +1,36 @@
-from app import create_app
 
+from app import create_app
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
+from sentry_sdk.integrations.celery import CeleryIntegration
+
+sentry_sdk.init(
+     dsn="https://redacted@example.com/6320262",
+    integrations=[FlaskIntegration(),RedisIntegration(),CeleryIntegration()],
+    traces_sample_rate=1.0
+    
+)
 
 app=create_app("development")
 
 if __name__== '__main__':
     app.run()
 
+# @app.route('/debug-sentry')
+# def traces_sampler(sampling_context):
+#     # ...
+#     # return a number between 0 and 1 or a boolean
 
+#     sentry_sdk.init(
+#     dsn="https://redacted@example.com/0",
+
+#     # To set a uniform sample rate
+#     traces_sample_rate=0.2,
+
+#     # Alternatively, to control sampling dynamically
+#     traces_sampler=traces_sampler
+# )
 
 # celery_app=make_celery(app)
 # app.run(host="0.0.0.0",port=443,ssl_context=("domain.crt","domain.key"))
