a590f0009d34cf6fac65775ce45cac39aea86228
subburajan-perumal <subburajanperumal@gmail.com>
2022-04-14 11:07:52 +0530

    pep8  on app util except e501

--- app/util/request_handler.py
+++ app/util/request_handler.py
@@ -1,53 +1,43 @@
 import json
-from config import required_field
 from flask import Request
-def find_format(P_request:Request):
+
+
+def find_format(P_request: Request):
     try:
 
         print("fomat finder working")
-        content_type=P_request.content_type.split("/")
-        req_data={}
+        content_type = P_request.content_type.split("/")
+        req_data = {}
 
-        if "x-www-form-urlencoded" in content_type :
-                print("default post P_request")
-                print(P_request)   
-                req_data =P_request.get_data()
-                
+        if "x-www-form-urlencoded" in content_type:
+            print("default post P_request")
+            print(P_request)
+            req_data = P_request.get_data()
 
         elif "form-data" in content_type:
-            tempdata=P_request.get_data()
-            req_data=json.loads(tempdata.decode())
-
-
-        # elif "xml" in content_type :
-        #     print("xml")
-        #     tempdata=P_request.get_data()
-        #     req_data=xmltodict.parse(tempdata)
-        #     print(req_data)
-            
+            tempdata = P_request.get_data()
+            req_data = json.loads(tempdata.decode())
 
         elif "json" in content_type:
             if(type(P_request.get_json()) is dict):
-                req_data=P_request.get_json()
-                if 'phone' in req_data and 'email' in req_data and 'lead_id'  in req_data and 'name' in req_data:
-                # if all(required_field in key for key in P_request.get_json()):
+                req_data = P_request.get_json()
+                if 'phone' in req_data and 'email' in req_data and 'lead_id' in req_data and 'name' in req_data:
                     return req_data
                 else:
-                    req_data={}
+                    req_data = {}
                     print(req_data)
                 # data=''.join(key for key,value in data if key.isalnum())
-            #multiple json in single P_request
-            elif(type(P_request.get_json())==list):
+            # multiple json in single P_request
+            elif(type(P_request.get_json()) == list):
                 print("json array")
                 # for _ in P_request.get_json():
 
         else:
-            req_data={}
+            req_data = {}
         return req_data
-    
+
     except Exception as e:
         print(str(e))
         print("problem in finding data type")
-        req_data={}
+        req_data = {}
         return req_data
-    
--- app/util/utility.py
+++ app/util/utility.py
@@ -1,96 +1,95 @@
-def cmpstring(string1,string2):
-    str1="".join([i for i in string1 if i.isalpha()])
-    str2="".join([i for i in string2 if i.isalpha()])
-    return str1==str2
+def cmpstring(string1, string2):
+    str1 = "".join([i for i in string1 if i.isalpha()])
+    str2 = "".join([i for i in string2 if i.isalpha()])
+    return str1 == str2
 
 
 def getTime():
-            from datetime import datetime
-            import pytz
-            now = datetime.now(pytz.timezone('Asia/Kolkata')) 
-            #print("now =", now)
-            # dd/mm/YY H:M:S
-            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-            return dt_string
+    from datetime import datetime
+    import pytz
+    now = datetime.now(pytz.timezone('Asia/Kolkata'))
+    # print("now =", now)
+    # dd/mm/YY H:M:S
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    return dt_string
 
-def getPhonenumber(numberlist:list):
+
+def getPhonenumber(numberlist: list):
     import phonenumbers as PN
-    filter_number=[]
-    numberstring="".join(str(i) for i in numberlist)
-    numbertext=PN.PhoneNumberMatcher(str(numberlist),None)
+    filter_number = []
+    # numberstring = "".join(str(i) for i in numberlist)
+    numbertext = PN.PhoneNumberMatcher(str(numberlist), None)
     # print(numbertext.has_next())
 
     for number in numbertext:
         print(filter_number.append(number.raw_string))
-   
-    
+
     for number in numberlist:
-        if len(number)==10:
+        if len(number) == 10:
             filter_number.append("+91"+number)
-            print("filtered_array : ",filter_number)
-    if len(filter_number)>0:
+            print("filtered_array : ", filter_number)
+    if len(filter_number) > 0:
         return(filter_number[0])
     else:
         return None
 
 
 def getName(name):
-    splited_name=name.split(" ")
-    
-    if len(splited_name)==1:
+    splited_name = name.split(" ")
 
-        return splited_name[0],splited_name[0]
-    elif len(splited_name)==2:
-        return splited_name[0],splited_name[1]
-    elif len(splited_name)==3:
-        return splited_name[0]+splited_name[1],splited_name[2]
+    if len(splited_name) == 1:
+
+        return splited_name[0], splited_name[0]
+    elif len(splited_name) == 2:
+        return splited_name[0], splited_name[1]
+    elif len(splited_name) == 3:
+        return splited_name[0]+splited_name[1], splited_name[2]
     else:
         return "" ""
-            
-#pre_akshaya_Adityaram_phase_5_krishnamoorthy perumal.png
-#(site,sub_projectname,path,leadname)
-def getsavePath(path,site_name,sub_project_name,leadname):
-    from datetime import date
-    # today = date.today()
-# ddmmYY
-    # formateddate = today.strftime("%d%m%y")
-    # print("d1 =", d1)
 
-    return [str(str(path) + '/' + "pre" + "_" + site_name + "_" + str(sub_project_name)+"_"+str(leadname).replace(" ",'_') + ".png"), 
-    str(str(path) + '/' + "post" + "_" + site_name + "_" + str(sub_project_name)+"_"+str(leadname).replace(" ","_") + ".png"),
-    str(str(path) + '/' + "err" + "_" + site_name + "_" + str(sub_project_name)+"_"+str(leadname).replace(" ","_") + ".png")
-    ]
+# pre_akshaya_Adityaram_phase_5_krishnamoorthy perumal.png
+# (site,sub_projectname,path,leadname)
+
+
+def getsavePath(path, site_name, sub_project_name, leadname):
+    return [
+        str(str(path) + '/' + "pre" + "_" + site_name + "_" + str(sub_project_name)+"_"+str(leadname).replace(" ", '_') + ".png"),
+        str(str(path) + '/' + "post" + "_" + site_name + "_" + str(sub_project_name)+"_"+str(leadname).replace(" ", "_") + ".png"),
+        str(str(path) + '/' + "err" + "_" + site_name + "_" + str(sub_project_name)+"_"+str(leadname).replace(" ", "_") + ".png")
+        ]
 # print(getsavePath("akshaya","Tango"))+str(leadname).replace(" ","_")
-
 
 
 def send_mail(lead_id, path, sub_project_name, name1):
     import subprocess
-    ### EXAMPLE CURL
+    # EXAMPLE CURL
     # curl -s --user REDACTED_SECRET_7 https://api.mailgun.net/v3/REDACTED_MAILGUN_DOMAIN/messages -F from='Automation Error <mailgun@REDACTED_MAILGUN_DOMAIN>' -F to=redacted@example.com -F to=redacted@example.com -F subject="Error. Testing Test - Mithila" -F text="Error. Lead registration failure.Lead ID: 920786000201726133 Project: Mithila Name: Testing Test" -F attachment=@"./krishnagrp/Testing Test_+919098124992738_Krishna Mithila.png"
-    Mailgun={"MAILGUN_DOMAIN":"REDACTED_MAILGUN_DOMAIN",
-"MAILGUN_URL":"https://api.mailgun.net/v3/REDACTED_MAILGUN_DOMAIN/messages",
-"MAILGUN_KEY":"REDACTED_SECRET_8",
-"FROM_MAIL":"mailgun@REDACTED_MAILGUN_DOMAIN",
-"TO_MAIL":["redacted@example.com","redacted@example.com"],
-# "TO_MAIL":["redacted@example.com","redacted@example.com"],
-"PASS":"REDACTED",
-"NAME":"Mailgun"}
+    Mailgun = {
+        "MAILGUN_DOMAIN": "REDACTED_MAILGUN_DOMAIN",
+        "MAILGUN_URL": "https://api.mailgun.net/v3/REDACTED_MAILGUN_DOMAIN/messages",
+        "MAILGUN_KEY": "REDACTED_SECRET_8",
+        "FROM_MAIL": "mailgun@REDACTED_MAILGUN_DOMAIN",
+        "TO_MAIL": ["redacted@example.com", "redacted@example.com"],
+        # "TO_MAIL":["redacted@example.com","redacted@example.com"],
+        "PASS": "REDACTED",
+        "NAME": "Mailgun"
+        }
     try:
         print("Sending mail...")
-        subject = "Error. {} - {}".format(name1, sub_project_name) 
+        subject = "Error. {} - {}".format(name1, sub_project_name)
         text = "Error. Lead registration failure. Lead ID: {} Project: {} Name: {}".format(lead_id, sub_project_name, name1)
-        
-        curlurl = "curl -s --user 'api:{}' {} -F from='Automation Error <mailgun@{}>' -F to={} -F to={} -F subject='{}' -F text='{}' -F attachment=@'{}'".format(Mailgun['MAILGUN_KEY'], Mailgun['MAILGUN_URL'],Mailgun['MAILGUN_DOMAIN'], Mailgun['TO_MAIL'][0], Mailgun['TO_MAIL'][1], subject, text, path)
-        
-        s , o = subprocess.getstatusoutput(curlurl)
-        print(s,o,sep="\n")
+
+        curlurl = "curl -s --user 'api:{}' {} -F from='Automation Error <mailgun@{}>' -F to={} -F to={} -F subject='{}' -F text='{}' -F attachment=@'{}'".format(Mailgun['MAILGUN_KEY'], Mailgun['MAILGUN_URL'], Mailgun['MAILGUN_DOMAIN'], Mailgun['TO_MAIL'][0], Mailgun['TO_MAIL'][1], subject, text, path)
+
+        s, o = subprocess.getstatusoutput(curlurl)
+        print(s, o, sep="\n")
 
     except Exception as e:
-        print("Exception occured due to: ",str(e))
+        print("Exception occured due to: ", str(e))
         print("Exception in sending mail...")
-    
+
     return
+
 
 def upload_an_attachment(lead_id, path):
     import requests
@@ -98,33 +97,33 @@ def upload_an_attachment(lead_id, path):
     import subprocess
     import os
 
-    zoho={"URL":"https://www.zohoapis.com/crm/v2/Leads/",
-    "CLIENT_ID":"REDACTED",
-    "CLIENT_SECRET":"REDACTED",
-    "REFRESH_TOKEN":"REDACTED",
-    "REDIRECT_URI":"https://example.com",
-    "NAME":"Zoho"}
-    
+    zoho = {
+        "URL": "https://www.zohoapis.com/crm/v2/Leads/",
+        "CLIENT_ID": "REDACTED",
+        "CLIENT_SECRET": "REDACTED",
+        "REFRESH_TOKEN": "REDACTED",
+        "REDIRECT_URI": "https://example.com",
+        "NAME": "Zoho"}
 
     try:
         url = 'https://accounts.zoho.com/oauth/v2/token?client_id={}&client_secret={}&refresh_token={}&grant_type=refresh_token'.format(
-        zoho['CLIENT_ID'], 
-        zoho['CLIENT_SECRET'], 
-        zoho['REFRESH_TOKEN'])
+                zoho['CLIENT_ID'],
+                zoho['CLIENT_SECRET'],
+                zoho['REFRESH_TOKEN'])
 
         headers = CaseInsensitiveDict()
         headers["Content-Length"] = "0"
         resp = requests.post(url, headers=headers)
         output = resp.json()
         print(output)
-        print("before :",os.environ.get("access-token"))
+        print("before :", os.environ.get("access-token"))
         access_token = output['access_token']
         os.environ["access_token"] = str(access_token)
-        
+
         access_token = os.environ.get("access_token")
-        print("after :",access_token)
+        print("after :", access_token)
         # print(os.get(['access_token']))
-    except:
+    except Exception:
         try:
             access_token = os.environ.get("access_token")
             print(access_token)
@@ -136,10 +135,10 @@ def upload_an_attachment(lead_id, path):
             lead_id,
             access_token,
             path)
-        out1,out2 = subprocess.getstatusoutput(CurlUrl)
-        print(out1,out2)
-    except:
-        print("Failed to Upload...")  
+        out1, out2 = subprocess.getstatusoutput(CurlUrl)
+        print(out1, out2)
+    except Exception:
+        print("Failed to Upload...")
 
     return
     # if __name__=="__main__":
