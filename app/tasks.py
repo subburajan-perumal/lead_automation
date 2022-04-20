b4d5322c670e1d01dbcfe1b0db2f1081ea991884
subburajan-perumal <subburajanperumal@gmail.com>
2022-04-20 03:58:36 +0000

    Modified None (default) added commands

--- app/tasks.py
+++ app/tasks.py
@@ -33,8 +33,8 @@ def lead(**lead_data):
         LA.create_lead()
         for _ in keyword_field:
             LA.get_keywords(lead_data.get(_["field"], ""), ";")
-        if len(keyword_field.strip()) == 0:
-	        LA.get_keywords("None (default)", ";")
+        if len(keyword_field) == 0:
+            LA.get_keywords("None (default)", ";")
         site_list = LA.search_by_keyword()
         site_list = json.loads(json_util.dumps(site_list))
         task_list = []
