33677f74950f036f8cae2743b57eb8659400aff0
subburajan-perumal <subburajanperumal@gmail.com>
2022-10-15 01:01:32 +0530

    project issue sorted again

--- app/views/lead.py
+++ app/views/lead.py
@@ -51,25 +51,26 @@ def lead_today():
     try:
         db=mongo.db
         lead_all = db.leads.find({"project":{"$exists":"true"}},{"_id":0}).sort("_id",-1).limit(50)
-        lead_all=json.loads(json_util.dumps(lead_all))
+        result=json.loads(json_util.dumps(lead_all))
+        return render_template("/lead/view.html",data=result)
 
-        leads_details = []
-        try:
-            for lead in lead_all:
-                arr = []
-                for project in lead['project']:
-                    lead_detail = dict()
-                    for key, value in lead.items():
-                        if key != 'project' and key != 'created_at' and key != 'modified_time':
-                            lead_detail[key] = value
-                        if key == 'created_at':
-                            lead_detail[key] = value['$date']
-                        if key == 'modified_time':
-                            lead_detail[key] = value['$date']
-                    for key, value in project.items():
-                        lead_detail[key] = value
-                    leads_details.append(lead_detail)
-            leads_details = json.loads(json_util.dumps(leads_details))
+        # leads_details = []
+        # try:
+        #     for lead in lead_all:
+        #         arr = []
+        #         for project in lead['project']:
+        #             lead_detail = dict()
+        #             for key, value in lead.items():
+        #                 if key != 'project' and key != 'created_at' and key != 'modified_time':
+        #                     lead_detail[key] = value
+        #                 if key == 'created_at':
+        #                     lead_detail[key] = value['$date']
+        #                 if key == 'modified_time':
+        #                     lead_detail[key] = value['$date']
+        #             for key, value in project.items():
+        #                 lead_detail[key] = value
+        #             leads_details.append(lead_detail)
+        #     leads_details = json.loads(json_util.dumps(leads_details))
 
 #only to print subproject in project
             #             if key == 'subproject':
@@ -77,9 +78,9 @@ def lead_today():
             #     lead_detail['project'] = arr
             #     leads_details.append(lead_detail)
             # leads_details = json.loads(json_util.dumps(leads_details))
-        except:
-            leads_details = lead_all
-        return render_template("/lead/view.html",data=leads_details)
+        # except:
+        #     leads_details = lead_all
+        # return render_template("/lead/view.html",data=leads_details)
 
     except Exception as e:
         return(str(e))
