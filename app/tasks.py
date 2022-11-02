9b4ae461f48bcd1a97ac435403a3538045aa0eaa
subburajan-perumal <subburajanperumal@gmail.com>
2022-11-02 10:48:43 +0530

    Update Magicbricks API sync
    - update app/tasks.py (run_magicbricks_api)

--- app/tasks.py
+++ app/tasks.py
@@ -284,8 +284,8 @@ def run_magicbricks_api():
 
         print('Magicbricks')
 
-        today = datetime.now() + timedelta(days=1)
-        yesterday = today - timedelta(days=2)
+        today = datetime.now() + timedelta(days=2)
+        yesterday = today - timedelta(days=4)
         today = today.astimezone(pytz.timezone('Asia/Kolkata'))
         yesterday = yesterday.astimezone(pytz.timezone('Asia/Kolkata'))
 
